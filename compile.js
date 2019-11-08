f87ac97769463c608538045032beeccc2441c679
strawberry <strawberry.wu@newtype.io>
2019-11-08 18:26:55 +0800

    初步实现compile

---
+++ compile.js
@@ -0,0 +1,62 @@
+class Compile {
+  constructor (el, vm) {
+    console.log(el,vm);
+    
+    this.$el = document.querySelector(el)
+    this.$vm = vm
+
+    if (this.$el) {
+      // 取出$el里面的元素加到文档片段中
+      this.$fragment = this.node2Fragment(this.$el)
+      // 执行编译
+      this.compile(this.$fragment)
+      // 将编译完的片段追加至$el尾部
+      this.$el.appendChild(this.$fragment)
+    }
+  }
+
+  node2Fragment (el) {
+    const frag = document.createDocumentFragment();
+      // 将el中所有子元素搬家至frag中
+      let child;
+      while ((child = el.firstChild)) {
+        frag.appendChild(child);
+      }
+      return frag;
+  }
+
+  compile (el) {
+    // 根据类型 {{}} s- @ 类型分别编译
+    const childNodes = el.childNodes
+    Array.from(childNodes).forEach(node => {
+      console.log(node)
+      if(node.nodeType === 1) {
+        //遍历元素属性 编译指令 事件
+        console.log(node)
+      } else if (node.nodeType === 3) {
+        // 编译{{}}里面的属性
+        console.log(node.textContent);
+        // this.compileText(node)
+      }
+
+      // 递归子节点 获取元素中的字节点
+      if (node.childNodes && node.childNodes.length > 0) {
+        this.compile(node);
+      }
+    })
+  }
+
+  compileText (node) {
+    console.log(node)
+    
+  }
+
+  isElementNode (node) {
+    return node.nodeType === 1
+  }
+
+  isInterpolation (node) {
+    return node.nodeType === 3 
+    // && /\{\{(.*)\}\}/.test(node.textContent)
+  }
+}
--- svue.js
+++ svue.js
@@ -2,22 +2,32 @@ class SVue {
   constructor (options) {
     this.$options = options
     this.$data = options.data
+    console.log(options)
     this.observe(this.$data)
+
+    new Compile(options.el, this)
+
+    // new Watcher()
+    // this.$data.test
   }
 
   observe (value) {
     if (!value || typeof value !== "object") return
 
     Object.keys(value).forEach(key => {
-      //  数据劫持 对每个对象增加get set属性
+      //  数据劫持 对data里面每个属性 增加get set方法
       this.defineReactive(value, key, value[key])
     })
   }
 
   defineReactive (obj, key, val) {
     this.observe(val)
+
+    const dep = new Dep()
+
     Object.defineProperty(obj, key, {
       get () {
+        Dep.target && dep.addDep(Dep.target)
         return val
       },
       set (newVal) {
@@ -25,10 +35,35 @@ class SVue {
           return
         }
         val = newVal
-        console.log(`${key}属性更新了：${val}`)
+        // console.log(`${key}属性更新了：${val}`)
+        dep.notify()
         
       }
     })
+  } 
+}
+
+//Dep: 管理watcher 收集依赖通知watcher更新
+class Dep {
+  constructor () {
+    this.deps = []
   }
-  
+
+  addDep(dep) {
+    this.deps.push(dep)
+  }
+
+  notify () {
+    this.deps.forEach(dep => dep.update())
+  }
+}
+
+// watcher 将watcher示例指定到Dep静态属性 通知视图更新
+class Watcher {
+  constructor () {
+    Dep.target = this
+  }
+  update () {
+    console.log('test 属性更新了')
+  }
 }
