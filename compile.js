a241e50b8d8b5c54cec7046b5184ed2d9d5493ac
strawberry <strawberry.wu@newtype.io>
2019-11-18 17:14:10 +0800

    编译指令和事件

--- compile.js
+++ compile.js
@@ -1,7 +1,5 @@
 class Compile {
   constructor (el, vm) {
-    console.log(el,vm);
-    
     this.$el = document.querySelector(el)
     this.$vm = vm
 
@@ -29,10 +27,22 @@ class Compile {
     // 根据类型 {{}} s- @ 类型分别编译
     const childNodes = el.childNodes
     Array.from(childNodes).forEach(node => {
-      console.log(node)
       if(node.nodeType === 1) {
         //遍历元素属性 编译指令 事件
-        console.log(node)
+        const nodeAttrs = node.attributes
+        Array.from(nodeAttrs).forEach(attr => {
+          const attrName = attr.name
+          const exp = attr.value
+          if (this.isDirective(attrName)) {
+            const dir = attrName.substring(2)
+            this[dir] && this[dir](node, this.$vm, exp)
+          }
+          if (this.isEvent(attrName)) {
+
+            const dir = attrName.substring(1)
+            this.eventHandler(node, this.$vm, exp, dir)
+          }
+        })
       } else if (node.nodeType === 3 && /\{\{(.*)\}\}/.test(node.textContent)) {
         // 编译{{}}里面的属性
         // console.log(node.textContent);
@@ -46,27 +56,65 @@ class Compile {
     })
   }
 
+  // 编译指令@. 处理事件
+  eventHandler (node, vm, exp, dir) {
+    let fn = vm.$options.methods && vm.$options.methods[exp];
+    if (dir && fn) {
+      node.addEventListener(dir, fn.bind(vm))
+    }
+  }
+
   compileText (node) {
-    // console.log(node, this.$vm, RegExp.$1, 'text')
     this.update(node, this.$vm, RegExp.$1, 'text')
+  }
+
+  // s-model
+  model (node, vm, exp) {
+    this.update(node, vm, exp, 'model')
+
+    node.addEventListener('input', e => {
+      vm[exp] = e.target.value
+    })
+  }
+
+  // s-text
+  text (node, vm, exp) {
+    this.update(node, vm, exp, 'text')
+  }
+
+  // s-html
+  html (node, vm, exp) {
+    this.update(node, vm, exp, 'html')
   }
 
   // 中转站 更新函数
   update (node,vm,exp,dir) {
+    console.log(vm[exp])
     const updateFn = this[dir+'Updater']
-    updateFn && updateFn()
+    updateFn && updateFn(node, vm[exp])
+    // 
+    new Watcher(vm, exp, function(value) {
+      updateFn && updateFn(node, value)
+    })
   }
 
-  text (node, vm, exp) {
-
+  textUpdater (node, value) {
+    node.textContent = value
   }
 
-  isElementNode (node) {
-    return node.nodeType === 1
+  htmlUpdater (node, value) {
+    node.innerHTML = value
   }
 
-  isInterpolation (node) {
-    return node.nodeType === 3 
-    // && /\{\{(.*)\}\}/.test(node.textContent)
+  modelUpdater (node,newVal) {
+    node.value = newVal
+  }
+
+  isDirective (attr) {
+    return attr.indexOf('s-') === 0
+  }
+
+  isEvent (attr) {
+    return attr.indexOf('@') === 0
   }
 }
--- svue.js
+++ svue.js
@@ -2,13 +2,13 @@ class SVue {
   constructor (options) {
     this.$options = options
     this.$data = options.data
-    console.log(options)
     this.observe(this.$data)
 
     new Compile(options.el, this)
-
-    // new Watcher()
-    // this.$data.test
+    // 执行周期钩子函数
+    if (options.created) {
+      options.created.call(this)
+    }
   }
 
   observe (value) {
@@ -17,6 +17,19 @@ class SVue {
     Object.keys(value).forEach(key => {
       //  数据劫持 对data里面每个属性 增加get set方法
       this.defineReactive(value, key, value[key])
+      // 将data 上的属性代理到this上
+      this.proxyData(key)
+    })
+  }
+
+  proxyData (key) {
+    Object.defineProperty(this, key, {
+      get () {
+        return this.$data[key]
+      },
+      set (newVal) {
+        this.$data[key] = newVal
+      }
     })
   }
 
@@ -58,12 +71,20 @@ class Dep {
   }
 }
 
-// watcher 将watcher示例指定到Dep静态属性 通知视图更新
+// watcher  通知视图更新
 class Watcher {
-  constructor () {
+  constructor (vm, key, cb) {
+    this.vm = vm
+    this.key = key
+     this.cb = cb
+    // 将watcher示例指定到Dep静态属性 target
     Dep.target = this
+    // 触发getter 添加依赖
+    this.vm[this.key]
+    Dep.target = null;
   }
   update () {
-    console.log('test 属性更新了')
+    this.cb.call(this.vm, this.vm[this.key]);
+    // console.log('test 属性更新了')
   }
 }
